05a343dea1fb160fa4051ec8015f95fdbff8e734
pclevy <pclevybr@gmail.com>
2019-03-18 23:35:28 -0300

    second commit

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ import {AppRegistry} from 'react-native';
 import App from './src';
 import {name as appName} from './app.json';
 
+// testando alteração
 AppRegistry.registerComponent(appName, () => App);
